588610f5e0f8ca6f73b7a84da1fa6d8edd912280
roopmitra86 <roopmitra86@gmail.com>
2026-03-31 01:40:10 +0530

    First commit

---
+++ src/config/constants.ts
@@ -0,0 +1,8 @@
+
+export const DATABASE_NAME: string = "";
+export const DATABASE_HOST: string = "localhost";
+export const DATABASE_PORT: number = 5432;
+export const DATABASE_USERNAME: string = "";
+export const DATABASE_PASSWORD: string = "";
+
+export const DATABASE_SYNC: boolean = false;
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig, PostgreSqlDriver } from "@mikro-orm/postgresql";
+import { DATABASE_HOST, DATABASE_NAME, DATABASE_PASSWORD, DATABASE_PORT, DATABASE_SYNC, DATABASE_USERNAME } from "./config/constants";
+import { Migrator } from "@mikro-orm/migrations";
+import { TsMorphMetadataProvider } from "@mikro-orm/reflection";
+
+export const MIKROORM_CONFIG = {
+    extensions: [Migrator],
+    entities: ['./dist/**/*.entity.js'],
+    entitiesTs: ['./src/**/*.entity.ts'],
+    driver: PostgreSqlDriver,
+    dbName: DATABASE_NAME,
+    host: DATABASE_HOST,
+    port: DATABASE_PORT,
+    user: DATABASE_USERNAME,
+    password: DATABASE_PASSWORD,
+    logger: (log_message) => console.log(log_message),
+    debug: true,
+    allowedGlobalContext: true,
+    metadataProvider: TsMorphMetadataProvider,
+    autoLoadEntities: false,
+    synchronize: DATABASE_SYNC,
+    migrations: {
+        tableName: 'mikro_orm_migrations',
+        path: 'dist/migrations',
+        pathTs: 'migrations',
+        snapshot: true
+    }
+}
+
+export default defineConfig(MIKROORM_CONFIG);
